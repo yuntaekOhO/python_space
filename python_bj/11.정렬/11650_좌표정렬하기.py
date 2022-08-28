5b08e020baf5dd2867e095b0508c16b51a8a4ab7
yuntaek <oyt1996@naver.com>
2022-08-28 18:46:03 +0900

    11650_좌표정렬 성공

--- python_bj/11.정렬/11650_좌표정렬하기.py
+++ python_bj/11.정렬/11650_좌표정렬하기.py
@@ -17,6 +17,7 @@ n = int(sys.stdin.readline())
 for _ in range(n):
     point.append(list(map(int, sys.stdin.readline().split())))
 point.sort()
-print(point)
-# for i in range(n):
-#     for j in range(1, n):
+
+for i in point:
+    result = str(i[0])+' '+str(i[1])
+    print(result)
